a9ed37c1b41c19a037fb50849f382af991c0da92
Xavier Guzman <xaguzman@gmail.com>
2023-12-15 02:06:54 -0600

    restore kde auto start for ssh-add

--- dotfiles/config/old-autostart-scripts/ssh-add.sh
+++ dotfiles/config/old-autostart-scripts/ssh-add.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export SSH\_ASKPASS=/usr/bin/ksshaskpass
-ssh-add $HOME/.ssh/id_rsa $HOME/.ssh/id_corporate_rsa
+ssh-add $HOME/.ssh/id_rsa $HOME/.ssh/id_ed25519 $HOME/.ssh/id_ed25519_incode 
